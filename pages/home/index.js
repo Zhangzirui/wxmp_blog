f2014f5b6afd1d303c7170d094b36062de5536e2
张子睿 <zirui.zhang@qunar.com>
2018-10-26 11:33:55 +0800

    feat: 更改home页动画

--- pages/home/index.js
+++ pages/home/index.js
@@ -14,6 +14,7 @@ Page({
     onReady() {
         this.avatarAnimation = wx.createAnimation();
         this.nameAnimation = wx.createAnimation();
+        this.infoAnimation = wx.createAnimation();
     },
     onPageScroll(e) {
         this.scrollTop = e.scrollTop;
@@ -21,19 +22,27 @@ Page({
     },
     animation(scrollTop) {
         const ratio = this.ratio;
-
-        if (scrollTop <= 320 * ratio) {
-            this.avatarAnimation.translate(-(345 * scrollTop / 320), -170 * scrollTop / 320).step();
-            this.avatarAnimation.scale(1 - scrollTop / 320).step();
-            this.nameAnimation.translate(0, -288 * scrollTop / 320).step();
+        if (scrollTop <= 500 * ratio) {
+            if (scrollTop <= 220 * ratio) {
+                this.avatarAnimation.translate(0, -170 * scrollTop / 220 );
+                this.avatarAnimation.scale(1 - scrollTop / (220 * ratio * 2));
+                this.avatarAnimation.step();
+            } else {
+                this.avatarAnimation.translate(-(345 * (scrollTop - 220 * ratio) / 280 ), -170 * this.ratio).step();
+            }
+            this.nameAnimation.translate(0, -288 * scrollTop / 500).step();
+            this.infoAnimation.opacity(1 - scrollTop / (164 * ratio)).step();
         } else {
             this.nameAnimation.translate(0, -288 * this.ratio).step();
-            this.avatarAnimation.translate(-325 * this.ratio, -170 * this.ratio).step();
-            this.avatarAnimation.scale(0.5).step();
+            this.avatarAnimation.translate(-325 * this.ratio, -170 * this.ratio);
+            this.avatarAnimation.scale(0.5);
+            this.avatarAnimation.step();
+            this.infoAnimation.opacity(0).step();
         }
         this.setData({
             avatarAnimation: this.avatarAnimation.export(),
-            nameAnimation: this.nameAnimation.export()
+            nameAnimation: this.nameAnimation.export(),
+            infoAnimation: this.infoAnimation.export()
         });
     },
     toggleShare(e) {
